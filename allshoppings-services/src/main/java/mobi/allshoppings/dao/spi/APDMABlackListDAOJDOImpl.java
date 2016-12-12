6f16fffe69aebae3a67245ddd22eed174bde2b73
Matias Hapanowicz <mhapanow@hotmail.com>
2016-12-12 10:20:40 -0600

    APDMABlackList

---
+++ allshoppings-services/src/main/java/mobi/allshoppings/dao/spi/APDMABlackListDAOJDOImpl.java
@@ -0,0 +1,103 @@
+package mobi.allshoppings.dao.spi;
+
+import java.util.List;
+import java.util.Map;
+import java.util.logging.Logger;
+
+import javax.jdo.PersistenceManager;
+import javax.jdo.Query;
+
+import org.springframework.util.StringUtils;
+
+import com.inodes.datanucleus.model.Key;
+
+import mobi.allshoppings.dao.APDMABlackListDAO;
+import mobi.allshoppings.exception.ASException;
+import mobi.allshoppings.exception.ASExceptionHelper;
+import mobi.allshoppings.model.APDMABlackList;
+import mobi.allshoppings.tools.CollectionFactory;
+import mobi.allshoppings.tools.Range;
+
+public class APDMABlackListDAOJDOImpl extends GenericDAOJDO<APDMABlackList> implements APDMABlackListDAO {
+
+	@SuppressWarnings("unused")
+	private static final Logger log = Logger.getLogger(APDMABlackListDAOJDOImpl.class.getName());
+
+	public APDMABlackListDAOJDOImpl() {
+		super(APDMABlackList.class);
+	}
+
+	@Override
+	public Key createKey(APDMABlackList seed) throws ASException {
+		if(!StringUtils.hasText(seed.getMac()) || !StringUtils.hasText(seed.getEntityId())){
+			throw ASExceptionHelper.notAcceptedException();
+		}
+		return (Key)keyHelper.obtainKey(APDMABlackList.class, keyHelper.resolveKey(seed.getEntityId() 
+				+ ":" + seed.getEntityKind() + ":" + seed.getMac()));
+	}
+
+	@Override
+	public List<APDMABlackList> getUsingEntityIdAndRange(String entityId, Integer entityKind, Range range, String order, boolean detachable) throws ASException {
+		List<APDMABlackList> returnedObjs = CollectionFactory.createList();
+
+		PersistenceManager pm;
+		pm = DAOJDOPersistentManagerFactory.get().getPersistenceManager();
+
+		try{
+			Map<String, Object> parameters = CollectionFactory.createMap();
+			List<String> declaredParams = CollectionFactory.createList();
+			List<String> filters = CollectionFactory.createList();
+
+			Query query = pm.newQuery(clazz);
+
+			// Store id parameter
+			if( StringUtils.hasText(entityId) ) {
+				declaredParams.add("String entityIdParam");
+				filters.add("entityId == entityIdParam");
+				parameters.put("entityIdParam", entityId);
+			}
+			
+			// Date parameter
+			if( entityKind != null ) {
+				declaredParams.add("Integer entityKindParam");
+				filters.add("entityKind >= entityKindParam");
+				parameters.put("entityKindParam", entityKind);
+			}
+
+			query.declareParameters(toParameterList(declaredParams));
+			query.setFilter(toWellParametrizedFilter(filters));
+
+			// Adds a cursor to the ranged query
+			if( range != null ) 
+				query.setRange(range.getFrom(), range.getTo());
+
+			// Sets order as required
+			if( StringUtils.hasText(order)) query.setOrdering(order);
+
+			@SuppressWarnings("unchecked")
+			List<APDMABlackList> objs = parameters.size() > 0 ? (List<APDMABlackList>)query.executeWithMap(parameters) : (List<APDMABlackList>)query.execute();
+			if (objs != null) {
+				// force to read
+				for (APDMABlackList obj : objs) {
+					if( detachable )
+						returnedObjs.add(pm.detachCopy(obj));
+					else
+						returnedObjs.add(obj);
+				}
+			}
+
+		} catch(Exception e) {
+			if(!( e instanceof ASException )) {
+				throw ASExceptionHelper.defaultException(e.getMessage(), e);
+			} else {
+				throw e;
+			}
+		} finally  {
+			pm.close();
+		}
+
+		return returnedObjs;
+
+	}
+
+}
---
+++ allshoppings-services/src/main/java/mobi/allshoppings/model/APDMABlackList.java
@@ -0,0 +1,200 @@
+package mobi.allshoppings.model;
+
+import java.io.Serializable;
+import java.util.Date;
+
+import javax.jdo.annotations.IdGeneratorStrategy;
+import javax.jdo.annotations.PersistenceCapable;
+import javax.jdo.annotations.Persistent;
+import javax.jdo.annotations.PrimaryKey;
+
+import com.inodes.datanucleus.model.Key;
+
+import mobi.allshoppings.model.interfaces.Identificable;
+import mobi.allshoppings.model.interfaces.ModelKey;
+
+@PersistenceCapable(detachable="true")
+public class APDMABlackList implements ModelKey, Serializable, Identificable {
+
+	private static final long serialVersionUID = 1L;
+	
+	@PrimaryKey
+    @Persistent(valueStrategy = IdGeneratorStrategy.UNSPECIFIED)
+	private Key key;
+
+	private String entityId;
+	private Integer entityKind;
+	private String mac;
+	private String description;
+	private Date creationDateTime;
+	private Date lastUpdate;
+
+	public APDMABlackList() {
+		super();
+		this.creationDateTime = new Date();
+	}
+	
+	/**
+	 * @return this entity key
+	 */
+	public String getIdentifier() {
+		return this.getKey() != null ? this.getKey().getName() : "";
+	}
+
+	/**
+	 * @return the key
+	 */
+	public Key getKey() {
+		return key;
+	}
+
+	/**
+	 * @param key the key to set
+	 */
+	public void setKey(Key key) {
+		this.key = key;
+	}
+
+	@Override
+	public void preStore() {
+		this.lastUpdate = new Date();
+	}
+
+	/**
+	 * @return the entityId
+	 */
+	public String getEntityId() {
+		return entityId;
+	}
+
+	/**
+	 * @param entityId the entityId to set
+	 */
+	public void setEntityId(String entityId) {
+		this.entityId = entityId;
+	}
+
+	/**
+	 * @return the entityKind
+	 */
+	public Integer getEntityKind() {
+		return entityKind;
+	}
+
+	/**
+	 * @param entityKind the entityKind to set
+	 */
+	public void setEntityKind(Integer entityKind) {
+		this.entityKind = entityKind;
+	}
+
+	/**
+	 * @return the mac
+	 */
+	public String getMac() {
+		return mac;
+	}
+
+	/**
+	 * @param mac the mac to set
+	 */
+	public void setMac(String mac) {
+		this.mac = mac;
+	}
+
+	/**
+	 * @return the description
+	 */
+	public String getDescription() {
+		return description;
+	}
+
+	/**
+	 * @param description the description to set
+	 */
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	/**
+	 * @return the creationDateTime
+	 */
+	public Date getCreationDateTime() {
+		return creationDateTime;
+	}
+
+	/**
+	 * @param creationDateTime the creationDateTime to set
+	 */
+	public void setCreationDateTime(Date creationDateTime) {
+		this.creationDateTime = creationDateTime;
+	}
+
+	/**
+	 * @return the lastUpdate
+	 */
+	public Date getLastUpdate() {
+		return lastUpdate;
+	}
+
+	/**
+	 * @param lastUpdate the lastUpdate to set
+	 */
+	public void setLastUpdate(Date lastUpdate) {
+		this.lastUpdate = lastUpdate;
+	}
+
+	/* (non-Javadoc)
+	 * @see java.lang.Object#hashCode()
+	 */
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((entityId == null) ? 0 : entityId.hashCode());
+		result = prime * result + ((entityKind == null) ? 0 : entityKind.hashCode());
+		result = prime * result + ((mac == null) ? 0 : mac.hashCode());
+		return result;
+	}
+
+	/* (non-Javadoc)
+	 * @see java.lang.Object#equals(java.lang.Object)
+	 */
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		APDMABlackList other = (APDMABlackList) obj;
+		if (entityId == null) {
+			if (other.entityId != null)
+				return false;
+		} else if (!entityId.equals(other.entityId))
+			return false;
+		if (entityKind == null) {
+			if (other.entityKind != null)
+				return false;
+		} else if (!entityKind.equals(other.entityKind))
+			return false;
+		if (mac == null) {
+			if (other.mac != null)
+				return false;
+		} else if (!mac.equals(other.mac))
+			return false;
+		return true;
+	}
+
+	/* (non-Javadoc)
+	 * @see java.lang.Object#toString()
+	 */
+	@Override
+	public String toString() {
+		return "APDMABlackList [key=" + key + ", entityId=" + entityId + ", entityKind=" + entityKind + ", mac=" + mac
+				+ ", description=" + description + ", creationDateTime=" + creationDateTime + ", lastUpdate="
+				+ lastUpdate + "]";
+	}
+
+}
