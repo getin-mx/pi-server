85ffd4aa7011f0c141a354501198d6ad88d186f4
DanielP <daniel@getin.mx>
2017-01-09 14:43:57 -0800

    Add test for FlorMapJourney

--- allshoppings-services/src/main/java/mobi/allshoppings/apdevice/FloorMapJourneyHelper.java
+++ allshoppings-services/src/main/java/mobi/allshoppings/apdevice/FloorMapJourneyHelper.java
@@ -2,14 +2,15 @@ package mobi.allshoppings.apdevice;
 
 import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import mobi.allshoppings.exception.ASException;
 
 public interface FloorMapJourneyHelper {
 	
-	public List<String> select(HashMap<Integer, String[]> mostValue);
+	public List<String> select(Map<Integer, List<String>> mostValue);
 	public HashMap<Integer, String[]> mostValuable(HashMap<Integer, String[]> map2);
-	public HashMap<Integer, List<String>> reverse(HashMap<String, Integer> map);
+	public Map<Integer, List<String>> reverse(HashMap<String, Integer> map);
 	public String merge(List<String> arr);
 	public void process() throws ASException;
 }
