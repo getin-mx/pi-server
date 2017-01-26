75a10e4cb64db877765af90dac238c1ea5aec642
Matias Hapanowicz <mhapanow@hotmail.com>
2017-01-25 22:54:26 -0600

    droc

--- allshoppings-cli/src/main/java/mobi/allshoppings/cli/ImportDRoc.java
+++ allshoppings-cli/src/main/java/mobi/allshoppings/cli/ImportDRoc.java
@@ -5,7 +5,6 @@ import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
-import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.List;
 import java.util.Map;
@@ -21,7 +20,6 @@ import mobi.allshoppings.exception.ASException;
 import mobi.allshoppings.exception.ASExceptionHelper;
 import mobi.allshoppings.model.ExternalAPHotspot;
 import mobi.allshoppings.tools.CollectionFactory;
-import oracle.security.xml.ws.addressing.bindings.To;
 
 
 public class ImportDRoc extends AbstractCLI {
@@ -31,7 +29,9 @@ public class ImportDRoc extends AbstractCLI {
 	public static OptionParser buildOptionParser(OptionParser base) {
 		if( base == null ) parser = new OptionParser();
 		else parser = base;
-		parser.accepts( "input", "Input Directory" ).withRequiredArg().ofType( String.class );
+		parser.accepts( "fromDate", "Date from" ).withRequiredArg().ofType( String.class );
+		parser.accepts( "toDate", "Date to" ).withRequiredArg().ofType( String.class );
+		parser.accepts( "fromLast", "Update from last request" ).withRequiredArg().ofType( Boolean.class );
 		return parser;
 	}
 
@@ -47,7 +47,21 @@ public class ImportDRoc extends AbstractCLI {
 			// Option parser help is in http://pholser.github.io/jopt-simple/examples.html
 			OptionSet options = parser.parse(args);
 
+			String fromDate = null;
+			String toDate = null;
+			Boolean fromLast = false;
+			
 			try {
+				
+				if(options.has("fromDate"))
+					fromDate = (String)options.valueOf("fromDate");
+				
+				if(options.has("toDate"))
+					toDate = (String)options.valueOf("toDate");
+				
+				if(options.has("fromLast"))
+					fromLast = (Boolean)options.valueOf("fromLast");
+				
 				if( options.has("help")) usage(parser);				
 			} catch( Exception e ) {
 				e.printStackTrace();
@@ -64,13 +78,14 @@ public class ImportDRoc extends AbstractCLI {
 			int EVENT_LEAVE = 1;
 			int EVENT_INVALID = 2;
 			
-			SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
 			String connectionString = "jdbc:mysql://173.224.112.51:3306/Syslog?user=getin&password=getindroc";
 			String query1 = "select distinct SysLogTag from SystemEvents where SysLogTag like '(\"U7LT,%' or SysLogTag like '(\"U2HSR,%'";
-			String queryca = "select count(*) from SystemEvents where SysLogTag in (";
+			String queryca = "select Count(*) from SystemEvents where ";
+			String queryca1 = " SysLogTag in (";
 			String querycb = ") and( Message like '%EVENT_STA_LEAVE%' or Message like '%EVENT_STA_JOIN%')";
 			String queryc = "";
-			String query2a = "select * from SystemEvents where SysLogTag in (";
+			String query2a = "select * from SystemEvents where ";
+			String query2a1 = " SysLogTag in (";
 			String query2b = ") and( Message like '%EVENT_STA_LEAVE%' or Message like '%EVENT_STA_JOIN%') limit 0,2000000";
 			String query2 = "";
 			List<String> hostnames = CollectionFactory.createList();
@@ -111,6 +126,24 @@ public class ImportDRoc extends AbstractCLI {
 					// Build Query Count
 					StringBuffer sb  = new StringBuffer();
 					sb.append(queryca);
+
+					if( fromLast ) {
+						
+					} else {
+						if( fromDate != null ) {
+							sb.append("ReceivedAt > timestamp('");
+							sb.append(fromDate);
+							sb.append(" 00:00:00') and ");
+						}
+						
+						if( toDate != null ) {
+							sb.append("ReceivedAt < timestamp('");
+							sb.append(toDate);
+							sb.append(" 00:00:00') and ");
+						}
+					}
+					
+					sb.append(queryca1);
 					boolean first = true;
 					for( String hostname : hostnames ) {
 						if(!first) sb.append(",");
@@ -143,6 +176,24 @@ public class ImportDRoc extends AbstractCLI {
 					// Build Query 2
 					StringBuffer sb  = new StringBuffer();
 					sb.append(query2a);
+
+					if( fromLast ) {
+						
+					} else {
+						if( fromDate != null ) {
+							sb.append("ReceivedAt > timestamp('");
+							sb.append(fromDate);
+							sb.append(" 00:00:00') and ");
+						}
+						
+						if( toDate != null ) {
+							sb.append("ReceivedAt < timestamp('");
+							sb.append(toDate);
+							sb.append(" 00:00:00') and ");
+						}
+					}
+					
+					sb.append(query2a1);
 					boolean first = true;
 					for( String hostname : hostnames ) {
 						if(!first) sb.append(",");
