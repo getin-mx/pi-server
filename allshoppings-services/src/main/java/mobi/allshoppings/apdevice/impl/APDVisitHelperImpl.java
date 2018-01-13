bb8efda0b8f4844ab46fcc17dd7c8ece7fef87c3
Nachintoch <ignacio@getin.mx>
2018-01-13 16:34:24 -0600

    Almost done improving reprocesses

--- allshoppings-services/src/main/java/mobi/allshoppings/apdevice/impl/APDVisitHelperImpl.java
+++ allshoppings-services/src/main/java/mobi/allshoppings/apdevice/impl/APDVisitHelperImpl.java
@@ -290,6 +290,7 @@ public class APDVisitHelperImpl implements APDVisitHelper {
 									final APHEntry entry = i.next();
 									// Employee check
 									if(!onlyEmployees || employeeListMacs.contains(entry.getMac().toUpperCase())) {
+										if(!assignmentsCache.containsKey(entry.getMac())) continue;
 										aux.clear();
 										aux.add(entry);
 										final List<APDVisit> visitList = aphEntryToVisits(aux, apdCache,
@@ -398,8 +399,15 @@ public class APDVisitHelperImpl implements APDVisitHelper {
 								}
 
 								log.log(Level.INFO, "Processing " + cache.size() + " APHEntries...");
+								boolean skip = false;
 								for(String mac :  cache.keySet()) {
 									if(cache.get(mac).isEmpty()) continue;
+									for(APHEntry aphe : cache.get(mac)) {
+										if(!assignmentsCache.containsKey(aphe.getMac())) {
+											skip = true;
+											break;
+										}
+									} if(skip) continue;
 									List<APDVisit> visitList = aphEntryToVisits(cache.get(mac), apdCache,
 											assignmentsCache, blackListMacs, employeeListMacs, tz, forDate);
 									for(APDVisit visit : visitList ) {
@@ -498,8 +506,19 @@ public class APDVisitHelperImpl implements APDVisitHelper {
 		
 		int timezoneOffset = tz.getOffset(startCal.getTimeInMillis());
 		
-		List<APHEntry> lastRes = CollectionFactory.createList();
-
+		String auxDate = null;
+		if(timezoneOffset > 0) {
+			startCal.add(Calendar.DATE, -1);
+			auxDate = sdf.format(startCal.getTime());
+			startCal.setTime(from);
+		}
+		
+		for(Iterator<APHEntry> i = lastRes.iterator(); i.hasNext();) {
+			if(timezoneOffset < 0 && forStringDate.compareTo(i.next().getDate()) > 0) i.remove();
+			else if(auxDate != null && auxDate.compareTo(i.next().getDate()) > 0) i.remove(); 
+		}
+		
+		APHEntry aphe;
 		if(timezoneOffset >= 0) {
 			startCal.add(Calendar.DATE, -1);
 			endCal.setTime(from);
