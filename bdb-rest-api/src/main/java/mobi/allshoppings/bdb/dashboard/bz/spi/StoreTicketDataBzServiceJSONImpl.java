b021d4252dd5d8739b637eeb2eaee23ba904bf2c
Nachintoch <ignacio@getin.mx>
2018-01-16 18:42:32 -0600

    Organized imports

--- bdb-rest-api/src/main/java/mobi/allshoppings/bdb/dashboard/bz/spi/StoreTicketDataBzServiceJSONImpl.java
+++ bdb-rest-api/src/main/java/mobi/allshoppings/bdb/dashboard/bz/spi/StoreTicketDataBzServiceJSONImpl.java
@@ -1,35 +1,15 @@
 package mobi.allshoppings.bdb.dashboard.bz.spi;
 
 
-import java.io.ByteArrayInputStream;
-import java.io.InputStream;
-import java.util.Date;
 import java.util.List;
-import java.util.Map;
-import java.util.logging.Level;
 
-import org.apache.poi.ss.usermodel.Cell;
-import org.apache.poi.ss.usermodel.CellType;
-import org.apache.poi.ss.usermodel.Row;
-import org.apache.poi.ss.usermodel.Sheet;
-import org.apache.poi.ss.usermodel.Workbook;
-import org.apache.poi.xssf.usermodel.XSSFWorkbook;
-import org.json.JSONArray;
-import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
-
-import com.ibm.icu.util.Calendar;
-import com.inodes.util.CollectionFactory;
 
 import mobi.allshoppings.dao.StoreTicketDAO;
 import mobi.allshoppings.exception.ASException;
 import mobi.allshoppings.exception.ASExceptionHelper;
-import mobi.allshoppings.model.Brand;
-import mobi.allshoppings.model.Image;
 import mobi.allshoppings.model.Store;
 import mobi.allshoppings.model.StoreTicket;
-import mobi.allshoppings.model.tools.StatusHelper;
-import mobi.allshoppings.tools.Range;
 import mx.getin.bdb.dashboard.bz.spi.StoreEntityData;
 import mx.getin.model.interfaces.StoreDataEntity;
 
