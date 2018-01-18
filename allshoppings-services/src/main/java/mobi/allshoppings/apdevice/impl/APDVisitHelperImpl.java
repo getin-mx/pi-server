40a0bf2682870a82b340cce6a8bd26cd396531a0
zero1416 <armengod96@gmail.com>
2018-01-17 19:40:20 -0600

    changes for devlyn class and preparation for the next changes and changes in class ExcelExport to add a uptime colum

--- allshoppings-services/src/main/java/mobi/allshoppings/apdevice/impl/APDVisitHelperImpl.java
+++ allshoppings-services/src/main/java/mobi/allshoppings/apdevice/impl/APDVisitHelperImpl.java
@@ -616,6 +616,7 @@ public class APDVisitHelperImpl implements APDVisitHelper {
 				}
 			}
 			map.put(mapped.getMac(), mapped);
+			
 		}
 
 		Iterator<APHEntry> ix = map.values().iterator();
@@ -1301,6 +1302,8 @@ public class APDVisitHelperImpl implements APDVisitHelper {
 		Long time = (visit.getCheckinFinished().getTime() -visit.getCheckinStarted().getTime()) / 60000;
 		
 		// TODO add minimum peasant time
+		
+		
 		
 		visit.setDuration(time);
 		return time <= 60 *60;
