a3f2b2627318f6e1ff318e95a6b55e088dbd0b65
Nachintoch <ignacio@getin.mx>
2018-02-01 18:49:45 -0600

    Fixed index process

--- allshoppings-cli/src/main/java/mx/getin/cli/IndexEntities.java
+++ allshoppings-cli/src/main/java/mx/getin/cli/IndexEntities.java
@@ -1,5 +1,6 @@
 package mx.getin.cli;
 
+import java.util.Date;
 import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -8,9 +9,14 @@ import org.springframework.context.ApplicationContext;
 
 import joptsimple.OptionParser;
 import mobi.allshoppings.cli.AbstractCLI;
+import mobi.allshoppings.dao.APDAssignationDAO;
 import mobi.allshoppings.dao.GenericDAO;
+import mobi.allshoppings.dao.InnerZoneDAO;
+import mobi.allshoppings.dao.StoreDAO;
 import mobi.allshoppings.exception.ASException;
+import mobi.allshoppings.model.APDAssignation;
 import mobi.allshoppings.model.APDevice;
+import mobi.allshoppings.model.EntityKind;
 import mobi.allshoppings.model.interfaces.ModelKey;
 import mobi.allshoppings.model.tools.IndexHelper;
 
@@ -35,13 +41,13 @@ public class IndexEntities extends AbstractCLI {
 		dao = getEntityDao("user.dao.ref");
 		LOG.log(Level.INFO, "Indexing users...");
 		index(dao.getAll(), dao.getIndexHelper());
-		dao = getEntityDao("store.dao.ref");
+		StoreDAO sDao = (StoreDAO) getApplicationContext().getBean("store.dao.ref");
 		LOG.log(Level.INFO, "Indexing stores...");
-		index(dao.getAll(), dao.getIndexHelper());
+		index(dao.getAll(), sDao.getIndexHelper());
 		dao = getEntityDao("apdevice.dao.ref");
 		List<ModelKey> devs = dao.getAll();
 		LOG.log(Level.INFO, "Completing Antennas' defaults");
-		updateAntennas(devs, dao);
+		updateAntennas(devs, dao, sDao);
 		LOG.log(Level.INFO, "Indexing APDevices...");
 		index(devs,  dao.getIndexHelper());
 		LOG.log(Level.INFO, "Process finished!");
@@ -61,11 +67,30 @@ public class IndexEntities extends AbstractCLI {
 		}
 	}
 	
-	private static void updateAntennas(List<ModelKey> devs, GenericDAO<ModelKey> dao) {
+	private static void updateAntennas(List<ModelKey> devs, GenericDAO<ModelKey> dao, StoreDAO storeDao) {
+		String id = null;
+		APDAssignationDAO assigDao = (APDAssignationDAO) getApplicationContext().getBean("apdassignation.dao.ref");
+		Date now = new Date();
+		InnerZoneDAO zoneDao = (InnerZoneDAO) getApplicationContext().getBean("innerzone.dao.ref");
 		for(ModelKey dev : devs) {
 			APDevice d = (APDevice) dev;
 			d.completeDefaults();
 			try {
+				List<APDAssignation> assigs = assigDao.getUsingHostnameAndDate(d.getHostname(), now);
+				if(assigs.isEmpty()) continue;
+				boolean inShopping = false;
+				for(APDAssignation assig : assigs) {
+					if(assig.getEntityKind() == EntityKind.KIND_INNER_ZONE) {
+						inShopping = true;
+						id = assig.getEntityId();
+						break;
+					}
+				}
+				d.setDescription(inShopping ? zoneDao.get(id).getName() :
+					storeDao.get(assigs.get(0).getEntityId()).getName());
+			} catch(ASException e) {
+				LOG.log(Level.WARNING, "Couldn't update antenna description", e);
+			} try {
 				dao.update(d);//TODO optimizar
 			} catch(ASException e) {
 				LOG.log(Level.WARNING, "Couldn't update antenna", e);
